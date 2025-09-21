40d2de3eec31aa4cbfe63b75f600b2ef8ec85d10
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-09-21 14:56:09 +0530

    Using streams naively

--- writeMany.js
+++ writeMany.js
@@ -40,19 +40,19 @@
 //CPU Usage: 100% (One Core)
 //Memory Usage: 50MB
 
-const fs = require("node:fs");
+// const fs = require("node:fs");
 
-(async()=>{
-    console.time("writeMany");
-    fs.open("test.txt", "w", (err, fd)=>{
-        for(let i = 0; i < 1000000; i++){
-            const buff = Buffer.from(` ${i} `, "utf-8")
-            fs.writeSync(fd, buff)
-        }
-    })
-    console.timeEnd("writeMany")
+// (async()=>{
+//     console.time("writeMany");
+//     fs.open("test.txt", "w", (err, fd)=>{
+//         for(let i = 0; i < 1000000; i++){
+//             const buff = Buffer.from(` ${i} `, "utf-8")
+//             fs.writeSync(fd, buff)
+//         }
+//     })
+//     console.timeEnd("writeMany")
 
-})()
+// })()
 
 
 
@@ -68,3 +68,26 @@ const fs = require("node:fs");
 //     console.timeEnd("writeMany")
 
 // })()
+
+
+
+
+const fs = require("node:fs/promises");
+
+//DON'T DO IT THIS WAY!!!
+// Execution Time: 270ms
+// CPU Usage: 100% (One Core)
+// Memory Usage: 200MB
+
+(async()=>{
+    console.time("writeMany")
+    const fileHandle = await fs.open("test.txt" , "w")
+
+    const stream = fileHandle.createWriteStream()
+
+    for(let i = 0; i < 1000000; i++){
+       const buff = Buffer.from(` ${i} `, "utf-8")
+       stream.write(buff)
+    }
+    console.timeEnd("writeMany")
+})()
