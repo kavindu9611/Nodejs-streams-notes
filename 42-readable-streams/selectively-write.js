5c337f1aba0a67d7af7983338cfd65e45b3dcd0d
kavindu9611 <kavinduwickramasinghe1920@gmail.com>
2025-10-02 15:49:33 +0530

    Fix:Selectively write

--- 42-readable-streams/selectively-write.js
+++ 42-readable-streams/selectively-write.js
@@ -1,6 +1,8 @@
 const fs = require("node:fs/promises");
 
 (async () => {
+  console.time("readBig");
+
   const fileHandleRead = await fs.open("bigFile.txt", "r");
   const fileHandleWrite = await fs.open("destBig.txt", "w");
 
@@ -9,16 +11,39 @@ const fs = require("node:fs/promises");
   });
   const streamWrite = fileHandleWrite.createWriteStream();
 
-  streamRead.on("data", (chunk) => {
-    const numbers = chunk.toString("utf-8").split("  ")
-    console.log(numbers)
-    if(!streamWrite.write(chunk)){
-      streamRead.pause()
-    }
-  });
-  
-  streamWrite.on("drain", ()=>[
-    streamRead.resume()
-  ])
+  let split = "";
 
+  streamRead.on("data", (chunk) => {
+    const numbers = chunk.toString("utf-8").split("  ");
+
+    if (Number(numbers[0]) !== Number(numbers[1]) - 1) {
+      if (split) numbers[0] = split.trim() + numbers[0].trim();
+    }
+
+    if (
+      Number(numbers[numbers.length - 2]) + 1 !==
+      Number(numbers[numbers.length - 1])
+    ) {
+      split = numbers.pop();
+    }
+
+    numbers.forEach((number) => {
+      let n = Number(number);
+
+      if (n % 2 === 0) {
+        if (!streamWrite.write(" " + n + " ")) {
+          streamRead.pause();
+        }
+      }
+    });
+  });
+
+  streamWrite.on("drain", () => {
+    streamRead.resume();
+  });
+
+  streamRead.on("end", ()=>{
+     console.log("Done reading")
+     console.timeEnd("readBig")
+  })
 })();
